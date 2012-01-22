6673188f0d1c5d30f64006713243ef8267061902
Krzysztof Kozmic <krzysztof@kozmic.pl>
2012-01-22 17:54:56 +1000

    - test to make sure mapping into new and preexisting objects of the same type can peacefully co-exist

--- src/Cartographer.Tests/MappingToExistingObjectTests.cs
+++ src/Cartographer.Tests/MappingToExistingObjectTests.cs
@@ -13,5 +13,16 @@
 
 			Assert.Equal("abc123", dto.Number);
 		}
+
+		[Fact]
+		public void Mapping_to_new_and_existing_objects_can_coexist()
+		{
+			var dto = new AccountDto();
+			mapper.Convert(new Account { Number = "abc123", Owner = new Person { Id = 42 } }, dto);
+			Assert.Equal("abc123", dto.Number);
+
+			dto = mapper.Convert<AccountDto>(new Account { Number = "abc123", Owner = new Person { Id = 42 } });
+			Assert.Equal("abc123", dto.Number);
+		}
 	}
 }
