db5845ca489b858aa557b6155dfb8afad4f353ac
Krzysztof Kozmic <krzysztof@kozmic.pl>
2012-01-29 12:57:22 +1000

    implemented ApprovalTests for Descriptor... need to wait for support for XUnit

---
+++ src/Cartographer.Tests/Internal/StringAssertEqualReporter.cs
@@ -0,0 +1,13 @@
+﻿namespace CartographerTests.Internal
+{
+	using ApprovalTests.Core;
+	using Xunit;
+
+	public class StringAssertEqualReporter: IApprovalFailureReporter
+	{
+		public void Report(string approved, string received)
+		{
+			Assert.Equal(approved, received);
+		}
+	}
+}
--- src/Cartographer.Tests/MappingDescriptorTests.cs
+++ src/Cartographer.Tests/MappingDescriptorTests.cs
@@ -1,14 +1,37 @@
 ﻿namespace CartographerTests
 {
+	using System;
+	using System.IO;
+	using System.Text;
+	using ApprovalTests;
+	using Cartographer;
 	using CartographerTests.Types;
 	using Xunit;
 
-	public class MappingDescriptorTests: AbstractMappingTests
+	public class MappingDescriptorTests:IDisposable
 	{
+		readonly IMapper mapper;
+
+		readonly StringBuilder output;
+
+		public MappingDescriptorTests()
+		{
+			output = new StringBuilder();
+			var builder = new MapperBuilder();
+			builder.Settings.MappingDescriptorWriter = new StringWriter(output);
+			mapper = builder.BuildMapper();
+		}
+
 		[Fact]
 		public void Can_describe_flattened_mapping()
 		{
 			mapper.Convert<AccountDto>(new Account { Number = "abc123", Owner = new Person { Id = 42 } });
+		}
+
+		public void Dispose()
+		{
+			//XUnit not yet supported in
+			//Approvals.Approve(output.ToString());
 		}
 	}
 }
