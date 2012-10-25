5c28b3f4365f1dcbbe4b4b3738883141c6ef64d0
Krzysztof Kozmic <krzysztof.kozmic@gmail.com>
2012-10-26 09:15:57 +1000

    added PCL project

--- src/Cartographer/Properties/AssemblyInfo.cs
+++ src/Cartographer/Properties/AssemblyInfo.cs
@@ -14,15 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("a6735928-a89e-4bc1-83ab-7a3c8a87cfe3")]
 
 // Version information for an assembly consists of the following four values:
 //
